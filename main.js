51e07b5b9a392e9ab74e15ba6419e4f3a1450032
unknown <Hyourinmaru80@gmail.com>
2018-08-27 12:28:05 +0300

    'v1.1'

--- main.js
+++ main.js
@@ -1,108 +1,115 @@
-const posts = [];
+const SpaceBook = function() {
+  posts = [] ;
+  currentId =  0;
+  $posts = $('.posts');
 
-let currentId = 0;
-const $posts = $('.posts');
-
-const createPost = function(text) {
-  const post = {
-    id: currentId,
-    text: text,
-    comments: new Array
-  }
-
-  currentId ++;
-  posts.push(post);
-}
-
-const createComment = function(userName, text) {
-  const comment = {
-    id: currentId,
-    text: text,
-    user: userName
-  }
-
-  currentId ++;
-  return comment;
-}
-
-const renderPosts = function () {
-  $posts.empty();
-  for (let i = 0; i < posts.length; i++) {
-   let $post =  $('<div class="post" data-id=' + posts[i].id + '><p><a href="#" class="remove-post">Remove</a> ' + posts[i].text + 
-    '</p> <div class="comments"></div> <form><input type="text" class="user-name" placeholder="User Name" /><input type="text" class="comment-text form-control" placeholder="Your Comment" /></form></div>');
-    addComments($post , posts[i])
-    $posts.append($post);
-  }
-}
-
-const addComments = function (post, currentPost) {
-  for (let i = 0; i < currentPost.comments.length; i++) {
-    post.find('.comments').append($('<p class="comment" data-id=' + currentPost.comments[i].id + '><a href="#" class="remove-comment">Remove</a> ' +
-    currentPost.comments[i].user + ': ' + currentPost.comments[i].text + ' </p>'))
-  }
-}
-
-const findPost = function (id) {
-  for (let i = 0; i < posts.length; i++) {
-    if (posts[i].id === id) {
-      return posts[i];
+  const _findPost = function (id) {
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === id) {
+        return posts[i];
+      }
     }
   }
-}
 
-const findComment = function (post , commentId) {
-  for (let i = 0; i < post.comments.length; i ++) {
-    if (commentId === post.comments[i].id) {
-      return post.comments[i];
+  const findComment = function (post , commentId) {
+    for (let i = 0; i < post.comments.length; i ++) {
+      if (commentId === post.comments[i].id) {
+        return post.comments[i];
+      }
     }
+  }
+
+  return {
+    createPost: function(text) {
+      const post = {
+        id: currentId,
+        text: text,
+        comments: new Array
+      }
+
+      currentId ++;
+      posts.push(post);
+    },
+
+    createComment: function(userName, text) {
+      const comment = {
+        id: currentId,
+        text: text,
+        user: userName
+      }
+
+      currentId ++;
+      return comment;
+    },
+
+    renderPosts: function () {
+      $posts.empty();
+      for (let i = 0; i < posts.length; i++) {
+      let $post =  $('<div class="post" data-id=' + posts[i].id + '><p><a href="#" class="remove-post">Remove</a> ' + posts[i].text + 
+        '</p> <div class="comments"></div> <form><input type="text" class="user-name" placeholder="User Name" /><input type="text" class="comment-text form-control" placeholder="Your Comment" /></form></div>');
+        this.addComments($post , posts[i])
+        $posts.append($post);
+      }
+    },
+
+    addComments: function (post, currentPost) {
+      for (let i = 0; i < currentPost.comments.length; i++) {
+        post.find('.comments').append($('<p class="comment" data-id=' + currentPost.comments[i].id + '><a href="#" class="remove-comment">Remove</a> ' +
+        currentPost.comments[i].user + ': ' + currentPost.comments[i].text + ' </p>'))
+      }
+    },
+
+    removePost: function (id) {
+      const post = _findPost(id);
+      posts.splice(posts.indexOf(post), 1);
+    },
+
+    removeComment: function (postId, commentId) {
+      const post = _findPost(postId);
+      const comment = findComment(post, commentId);
+      post.comments.splice(post.comments.indexOf(comment), 1);
+    },
   }
 }
 
-const removePost = function (id) {
-  const post = findPost(id);
-  posts.splice(posts.indexOf(post), 1);
-}
+const app = SpaceBook();
 
-const removeComment = function (postId, commentId) {
-  const post = findPost(postId);
-  const comment = findComment(post, commentId);
-  post.comments.splice(post.comments.indexOf(comment), 1);
-}
-
-$posts.on("click", ".remove-comment", function() {
+$(".posts").on("click", ".remove-comment", function() {
   let commentId = $(this).closest('.comment').data('id');
   let postId = $(this).closest('.post').data('id');
-  removeComment(postId, commentId);
-  renderPosts();
+  app.removeComment(postId, commentId);
+  app.renderPosts();
 })
 
-$posts.on("click", ".remove-post", function() {
+$(".posts").on("click", ".remove-post", function() {
   let id = $(this).closest(".post").data("id");
-  removePost(id);
-  renderPosts();
+  app.removePost(id);
+  app.renderPosts();
 })
 
 $(".add-post").on("click", function () {
   let text = $("#post-name").val();
-  createPost(text);
-  renderPosts();
+  app.createPost(text);
+  app.renderPosts();
 })
 
-$posts.on("keyup", ".comment-text", function () {
+$(".posts").on("keyup", ".comment-text", function () {
   if (event.keyCode === 13) {
     let id = $(this).closest(".post").data('id');
     let text = $(this).val();
     let user = $(this).closest(".post").find(".user-name").val();
-    findPost(id).comments.push(createComment(user, text));
-    renderPosts();
+    app.findPost(id).comments.push(app.createComment(user, text));
+    app.renderPosts();
   }
 })
 
 $(function(){
-  $('.posts').sortable();
-  $('.posts').disableSelection();
+  $(".posts").sortable();
+  $(".posts").disableSelection();
 })
 
-// $( function() {
-//   $( ".post" ).dialog();
-// } );
+$(".posts").on("click", ".post", function() {
+  
+  $(this).dialog();
+  app.renderPosts();
+});
